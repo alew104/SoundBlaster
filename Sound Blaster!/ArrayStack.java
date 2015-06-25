927cdcc9a180f7ffec434a18bfd31ede90db3376
Alex Lew <alew104@gmail.com>
2015-06-25 08:29:46 -0700

    Initial Commit

---
+++ Sound Blaster!/ArrayStack.java
@@ -0,0 +1,33 @@
+
+public class ArrayStack implements DStack {
+	
+	public ArrayStack() {
+		double soundValue[] = new double[10];
+	    // Your constructor code 
+	  }
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void push(double d) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double pop() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double peek() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ Sound Blaster!/ListStack.java
@@ -0,0 +1,32 @@
+
+public class ListStack implements DStack {
+	
+	public ListStack() {
+	    // Your constructor code 
+	  }
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void push(double d) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double pop() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double peek() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
