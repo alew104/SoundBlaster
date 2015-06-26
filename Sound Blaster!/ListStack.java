c7d0402550543b76f15d8f5a8fd8c70347a26dbb
Alex Lew <alew104@gmail.com>
2015-06-25 18:25:05 -0700

    commit liststack
    while loop implementation

--- Sound Blaster!/ListStack.java
+++ Sound Blaster!/ListStack.java
@@ -1,47 +1,93 @@
+import java.util.EmptyStackException;
 
 public class ListStack implements DStack {
 	public int top;
 	public ListStackNode soundValue;
 	public ListStack() {
 	    // Your constructor code 
-		 soundValue = new ListStackNode();
+		 soundValue = null;
 		 top = 0;
 	  }
 
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
+		if (soundValue == null){
+			return true;
+		}
+		
+		if (top == 0){
+			return true;
+		}
+		
 		return false;
 	}
 
 	@Override
 	public void push(double d) {
 		// TODO Auto-generated method stub
-		ListStackNode placeholder = traverseToNull(soundValue);
-		placeholder.next = new ListStackNode(d);
-		
-	}
-	
-	public ListStackNode traverseToNull(ListStackNode full){
-		if (full.next == null){
-			return full;
+		if (soundValue == null){
+			soundValue = new ListStackNode(d);
 		} else {
-			traverseToNull(full.next);
+			addLast(soundValue, d);
 		}
-		return null;
 	}
 
+	public void addLast(ListStackNode full, double d){
+		boolean added = false;
+		while (!added){
+			full = full.next;
+			if (full.next == null){
+				full.next = new ListStackNode(d);
+				top++;
+				added = true;
+			}
+		}
+	}
 	@Override
 	public double pop() {
 		// TODO Auto-generated method stub
-		
-		return 0;
+		if (top == 0){
+			throw new EmptyStackException();
+		}
+		return removeLast(soundValue);
+	}
+	
+	public double removeLast(ListStackNode full){
+		boolean removed = false;
+		double returnValue = 0.0;
+		while (!removed){
+			full = full.next;
+			if (full.next.next == null){
+				returnValue = full.next.data;
+				full.next = null;
+				top--;
+				removed = true;
+			}
+		}
+		return returnValue;
 	}
 
 	@Override
 	public double peek() {
 		// TODO Auto-generated method stub
-		return 0;
+		if (top == 0){
+			throw new EmptyStackException();
+		}
+		return checkLast(soundValue);
+	}
+	
+	public double checkLast(ListStackNode full){
+		boolean reachedLast = false;
+		double returnValue = 0.0;
+		while (!reachedLast){
+			full = full.next;
+			if (full.next == null){
+				returnValue = full.data;
+				reachedLast = true;
+			}
+		}
+		return returnValue;
 	}
 
 }
